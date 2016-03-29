e9c7c693383028f69f0bf7cf2866c686416db6c1
Denys <denys.ark@gmail.com>
2016-03-29 19:28:44 +0300

    integration of new changes

--- ViewModels/Menu/menuviewmodel.js
+++ ViewModels/Menu/menuviewmodel.js
@@ -1,6 +1,5 @@
 ChefPlaza.MenuViewModel = function (menuId) {
     var self = this;
-
     //document.getElementById('review').scrollIntoView();
 
     //self.priceRange = ko.observable("0,300");
@@ -12,24 +11,32 @@ ChefPlaza.MenuViewModel = function (menuId) {
     //self.maxPrice = ko.computed(function () {
     //    return self.priceRange().split(',')[1];
     //});
-
-
+    
     self.submitRequest = function (form) { 
         if ($(form).valid()) {
             $.ajax({
                 method: "post",
                 //url: ChefPlaza.basePath + "menu/requestmenu/" + menuId,
-                url: "fakerequest.js",
+                //url: ChefPlaza.basePath + "menu/newmenurequest/" + menuId,
+                url: "menu/newmenurequest/fakerequest.php",
                 data: $(form).serialize(),
                 success: function (data) {
                     $('#requestMenuDialog').modal('hide');
                     $('#requestMenuDialogConfirmation').modal('show');
                     //var url = $("#RedirectToThanks").val();
                     //location.href = url;
+                    console.info('request success');
+                    
+                },
+                error: function () {
+                    console.error('request not sent');
                 }
             });
         }
-    }
-
+    }//end submitRequest
+    
     return self;
-}
+    
+}//end ChefPlaza
+
+
